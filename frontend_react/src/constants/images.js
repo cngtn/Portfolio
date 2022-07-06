d324e2546332165a71d621795106acc776399db5
Đặng Công Tấn <congtan2503@gmail.com>
2022-07-06 10:54:35 +0700

    Sanity and React

--- frontend_react/src/constants/images.js
+++ frontend_react/src/constants/images.js
@@ -22,6 +22,8 @@ import about01 from '../assets/about01.png';
 import about02 from '../assets/about02.png';
 import about03 from '../assets/about03.png';
 import about04 from '../assets/about04.png';
+import about05 from '../assets/about05.png';
+import about06 from '../assets/about06.png';
 
 import profile from '../assets/profile.png';
 import circle from '../assets/circle.svg';
@@ -59,6 +61,8 @@ export default {
     about02,
     about03,
     about04,
+    about05,
+    about06,
     profile,
     circle,
     logo,
--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -4,23 +4,34 @@ import { motion } from 'framer-motion'
 import { images } from '../../constants'
 
 import './About.scss'
+import { urlFor, client } from '../../client'
 
-const abouts = [
-  {
-    title: 'Full-Stack Developer', decscription: 'I am a full-stack developer', imgUrl: images.about01
-  },
-  {
-    title: 'UX/UI', decscription: 'I am a full-stack developer', imgUrl: images.about02
-  },
-  {
-    title: 'Data Analyst', decscription: 'I am a full-stack developer', imgUrl: images.about03
-  },
-  {
-    title: 'Web Developer', decscription: 'I am a full-stack developer', imgUrl: images.about04
-  }
-]
+// const abouts = [
+//   {
+//     title: 'Full-Stack Developer', decscription: 'I am a full-stack developer', imgUrl: images.about01
+//   },
+//   {
+//     title: 'UX/UI', decscription: 'I am a full-stack developer', imgUrl: images.about02
+//   },
+//   {
+//     title: 'Data Analyst', decscription: 'I am a full-stack developer', imgUrl: images.about03
+//   },
+//   {
+//     title: 'Web Developer', decscription: 'I am a full-stack developer', imgUrl: images.about04
+//   }
+// ]
 
 const About = () => {
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+
+    client.fetch(query).then((data) => {
+      setAbouts(data);
+    });
+  }, []);
+
   return (
     <>
       <h2 className="head-text"> I Know That <span>Good Devs</span> <br /> means <span>Good Business</span>
@@ -30,13 +41,13 @@ const About = () => {
           <motion.div
             whileInView={{ opacity: 1 }}
             whileHover={{ scale: 1.1 }}
-            transition={{ duration: 1, type: 'tween' }}
+            transition={{ duration: 0.5, type: 'tween' }}
             className="app__profile-item"
             key={about.title + index}
           >
-            <img src={about.imgUrl} alt={about.title} />
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
             <h2 className="bold-text" style={{ marginTop: 20 }}>{about.title}</h2>
-            <p className="p-text" style={{ marginTop: 10 }}>{about.decscription}</p>
+            <p className="p-text" style={{ marginTop: 10 }}>{about.description}</p>
           </motion.div>
         ))}
       </div>
